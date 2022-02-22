a9e395ae66fd7e57d8b2204d4fb3675c3773d869
MaxwellChrist <ribthulu@gmail.com>
2022-02-22 15:14:49 -0500

    created membersList, imported useState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
-console.log("this is a test");
+const memberList = [
+  { name: "Alex", email: "gogogadget@yahoo.com", role: "UX Designer"},
+  { name: "Beth", email: "bethisawesome@gmail.com", role: "Frontend Engineer"},
+  { name: "Carl", email: "mediocrecoder@aol.com", role: "Backend Engineer"},
+  { name: "Danielle", email: "seattlebound@outlook.com", role: "Quality Assurance"},
+  { name: "Evan", email: "throwaway@zoho.com", role: "Director"}
+]
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <input 
+          name="name"
+          placeholder="Enter your name here"
+          value=""
+          type="text"
+        />
+        <input 
+          name="email"
+          placeholder="Enter your email here"
+          value=""
+          type="email"
+        />
+      </form>
     </div>
   );
 }
